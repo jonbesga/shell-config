ac0db5c379eaa4d50a1c2fc3f8af5aecc6a6fea3
Jon Ander Besga <jonan.bsg@gmail.com>
2018-05-26 17:14:08 +0100

    all done

---
+++ setup.sh
@@ -0,0 +1,7 @@
+mv .bashrc ~/.bashrc
+mv .tmux.conf ~/.tmux.conf
+mv .zshrc ~/.zshrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
