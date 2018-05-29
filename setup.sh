dd03952f9ba64f43ca8c3e5aa9dd1802f0c4914b
Jon Besga <jonan.bsg@gmail.com>
2018-05-29 21:06:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-mv .bashrc ~/.bashrc
-mv .tmux.conf ~/.tmux.conf
-mv .zshrc ~/.zshrc
-
+git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
+mv .bashrc ~/.bashrc
+mv .tmux.conf ~/.tmux.conf
+mv .zshrc ~/.zshrc
