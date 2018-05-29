449666f7720310998c4c9255c52d2a3e9db13a11
Jon Besga <jonan.bsg@gmail.com>
2018-05-29 21:18:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+sudo apt install zsh
 git clone https://github.com/jonbesga/shell-config .shell-config
 git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
